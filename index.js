c62ba8658e2f8816ba4cbb0f9a7c43a2ba667a2c
Hao Yuan <haomichaelyuan@gmail.com>
2022-09-08 09:12:07 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main=document.getElementById("main")
+console.log(main)
+const body=document.getElementsByTagName("body")[0]
+body.removeChild(main)
+
+const newHeader=document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.textContent="Hao Yuan is the champion"
