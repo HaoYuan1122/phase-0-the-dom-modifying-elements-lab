38ec1431c4124c456bf7e054204332723979825f
Hao Yuan <haomichaelyuan@gmail.com>
2022-12-13 11:42:02 +1100

    commit tree fa9607834b1bc68bf6c0231241cd0be73224dcff

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Write your code here!
-const main=document.getElementById("main")
-console.log(main)
-const body=document.getElementsByTagName("body")[0]
-body.removeChild(main)
+// const main=document.getElementById("main")
+// console.log(main)
+// const body=document.getElementsByTagName("body")[0]
+// body.removeChild(main)
 
-const newHeader=document.createElement("h1")
-newHeader.setAttribute("id","victory")
-newHeader.textContent="Hao Yuan is the champion"
+// const newHeader=document.createElement("h1")
+// newHeader.setAttribute("id","victory")
+// newHeader.textContent="Hao Yuan is the champion"
+
+let newHeader=document.createElement('h1')
+newHeader.id='victory'
+
+newHeader.textContent='Hao Yuan is the champion'
